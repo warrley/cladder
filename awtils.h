0c1c8e2665096903b4df364b5aa0369a33a38aac
warrley <warleeyfarias@gmail.com>
2025-07-23 12:12:17 -0300

    funcs

--- awtils.h
+++ awtils.h
@@ -1,5 +1,9 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef AWTILS_H
+#define AWTILS_H
+
+#define word_len 45
+#define tip_len 500
+#define num_word 4
 
 int awcmp(char *s1, char *s2);
 int awlen(char *s1);
--- second.c
+++ second.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "awtils.h"
 
 struct Word {
 	int id;
-	char string[501];
-	char tip[101];
+	char string[word_len + 1];
+	char tip[tip_len + 1];
 };
 
 typedef struct Word word;
 
-int main() {
-	printf("liste 5 palavras sendo diferentes apenas por uma letra\n");
-	word words[5];
-	
-	for(int i = 0; i < 5; i++) {
-	    printf("palavra %d: ", i+1);
-		scanf(" %s", words[i].string);
-	}
-	
-	for(int i = 0; i < 5; i++) {
-	    printf("\ndica da palavra %s: ", words[i].string);
-		scanf(" %s", words[i].tip);
-	}
+void insertWords(word *words, char *initial, int *level);
 
+int main() {
+	char initial[word_len + 1];
 	int level;
-	printf("\nescolha o nivel de dificuldade desse bloco de palavras");
-	while(1) {
-		printf("\n[1] - Fácil");
-		printf("\n[2] - Médio");
-		printf("\n[3] - Difícil\n");
-		scanf("%d", &level);
-		if(level < 1 || level > 3) printf("escolha um nivel valido.");
-		else 					   break;
-	}
+	word words[num_word];
+	
+	insertWords(words, initial, &level);
 
 	FILE *save = fopen("palavras.txt", "a");
 	
@@ -41,9 +26,9 @@ int main() {
 	    exit(1);
 	}
 
-	fprintf(save, "[\n\tid:%d\n\tlevel:%d\n", 0, level);
+	fprintf(save, "[\n\tid: %d\n\tlevel: %d\n\theader: %s\n", 0, level, initial);
 
-	for(int i = 0; i < 5; i++) {
+	for(int i = 0; i < num_word; i++) {
         fprintf(save, "\t{word: %s, tip: %s}\n", words[i].string, words[i].tip);
     }
 
@@ -53,4 +38,33 @@ int main() {
 	printf("palavras salvas com sucesso");
 
 	return 0;
+};
+
+void insertWords(word *words, char *initial, int *level) {
+	printf("digite a palavra inicial: ");
+	scanf(" %s", initial);
+	
+	printf("liste 4 palavras sendo diferentes apenas por uma letra\n");
+
+	for(int i = 0; i < num_word; i++) {
+		printf("palavra %d: ", i+1);
+		scanf(" %s", words[i].string);
+	}
+	
+	for(int i = 0; i < num_word; i++) {
+		printf("\ndica da palavra %s: ", words[i].string);
+		scanf(" %[^\n]", words[i].tip);
+	}
+	
+	int copyLevel;
+	printf("\nescolha o nivel de dificuldade desse bloco de palavras");
+	while(1) {
+		printf("\n[1] - Fácil");
+		printf("\n[2] - Médio");
+		printf("\n[3] - Difícil\n");
+		scanf("%d", &copyLevel);
+		if(copyLevel < 1 || copyLevel > 3) printf("escolha um nivel valido.");
+		else 					   break;
+	}
+	*level = copyLevel;
 };
