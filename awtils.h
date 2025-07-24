35cf7f6f75789fc1e4adf955e20055d1a66b5799
warrley <warleeyfarias@gmail.com>
2025-07-23 22:23:56 -0300

    validations

--- awtils.h
+++ awtils.h
@@ -5,7 +5,13 @@
 #define tip_len 500
 #define num_word 4
 
+struct Word;
+typedef struct Word word;
+
 int awcmp(char *s1, char *s2);
 int awlen(char *s1);
+void awcpy(char *s1, char *s2);
+int verifyWord(word *words, char *initial, int index);
+int diffLetter(char *word1, char *word2);
 
 #endif
--- second.c
+++ second.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "awtils.h"
 
 struct Word {
@@ -11,6 +12,7 @@ typedef struct Word word;
 
 void insertWords(word *words, char *initial, char *level);
 void writeFile(word *words, char *initial, char level);
+void clearScreen();
 
 int main() {
 	char initial[word_len + 1];
@@ -28,12 +30,20 @@ void insertWords(word *words, char *initial, char *level) {
 	printf("digite a palavra inicial: ");
 	scanf(" %s", initial);
 	
-	printf("liste 4 palavras sendo diferentes apenas por uma letra\n");
+	printf("liste %d palavras sendo diferentes apenas por uma letra\n", num_word);
 
 	for(int i = 0; i < num_word; i++) {
-		printf("palavra %d: ", i+1);
-		scanf(" %s", words[i].string);
-	}
+        clearScreen();
+        printf("--- Palavra %d de %d ---\n", i+1, num_word);
+        printf("Palavra anterior: %s\n", (i == 0) ? initial : words[i-1].string);
+        
+        do {
+			// sleep(1);
+			// clearScreen();
+            printf("\ndigite uma palavra que difere em 1 letra de %s: ", (i == 0) ? initial : words[i-1].string);
+            scanf(" %s", words[i].string);
+        } while(!verifyWord(words, initial, i));
+    }
 	
 	for(int i = 0; i < num_word; i++) {
 		printf("\ndica da palavra %s: ", words[i].string);
@@ -69,4 +79,12 @@ void writeFile(word *words, char *initial, char level){
 	
 	fclose(save);
 	printf("palavras salvas com sucesso");
+};
+
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 };
